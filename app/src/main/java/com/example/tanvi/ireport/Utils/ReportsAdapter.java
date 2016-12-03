03ced9fbe3ed7838dc1be19098bc9db2ac743e01
tanvi6593 <tanvishukla234@gmail.com>
2016-12-02 16:41:18 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/tanvi/ireport/Utils/ReportsAdapter.java
@@ -0,0 +1,93 @@
+package com.example.tanvi.ireport.Utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.tanvi.ireport.R;
+import com.example.tanvi.ireport.Model.Report_Item;
+
+import java.util.List;
+
+/**
+ * Created by tanvi on 12/2/2016.
+ */
+
+public class ReportsAdapter extends BaseAdapter {
+
+    Context context;
+    List<Report_Item> reportList;
+
+    public ReportsAdapter(Context context, List<Report_Item> reportList) {
+        this.context = context;
+        this.reportList = reportList;
+    }
+
+    private class ViewHolder{
+
+        ImageView imageView;
+        TextView textName;
+        TextView textStatus;
+        TextView textDateTime;
+
+    }
+
+
+    @Override
+    public int getCount() {
+        return reportList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+
+        return reportList.get(i);
+
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return reportList.indexOf(getItem(i));
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+
+
+        ViewHolder holder =null;
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+
+        if(convertView == null){
+
+            convertView = layoutInflater.inflate(R.layout.list_row,null);
+
+            holder = new ViewHolder();
+            holder.textName = (TextView) convertView.findViewById(R.id.reportName);
+            holder.textDateTime = (TextView) convertView.findViewById(R.id.dateTime);
+            holder.textStatus = (TextView) convertView.findViewById(R.id.status);
+            holder.imageView = (ImageView) convertView.findViewById(R.id.list_image);
+            convertView.setTag(holder);
+        }
+        else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Report_Item report = (Report_Item)getItem(position);
+        holder.textName.setText(report.getName());
+        holder.textDateTime.setText(report.getDateTime());
+        holder.textStatus.setText(report.getStatus());
+        holder.imageView.setImageResource(report.getImageURL());
+
+        return convertView;
+
+
+    }
+
+
+}
